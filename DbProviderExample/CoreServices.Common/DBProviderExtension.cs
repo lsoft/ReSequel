12e4a89b61b0720bbf9e58b421b259dfea2f4dd4
lsoft <lsoft@list.ru>
2020-02-16 19:45:57 +0500

    * defined a way to detect extension methods

---
+++ DbProviderExample/CoreServices.Common/DBProviderExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSoft.Server
+{
+    public static class DBProviderExtension
+    {
+        public static void ExecuteExtension(
+            this DBProvider dbProvider,
+            int unused1,
+            string sqlCommand,
+            int unused2
+            )
+        {
+
+        }
+    }
+}
---
+++ DbProviderExample/Handler/SomeHandler5.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LSoft.Server;
+
+namespace Hanlder
+{
+    public class SomeHandler5
+    {
+        public void CheckExtension()
+        {
+            var dbp = new DBProvider();
+
+            dbp.ExecuteExtension(
+                1,
+                "select 1 union select 2",
+                2);
+        }
+    }
+}
